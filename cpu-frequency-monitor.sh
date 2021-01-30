e43fc98aef5a62e11a883374942ba95a0542f82a
Sven <svennussgruber@gmail.com>
2021-01-30 10:53:56 +0000

    script to monitor CPU frequency in real time

--- cpu-frequency-monitor.sh
+++ cpu-frequency-monitor.sh
@@ -3,4 +3,6 @@
 #Lists all CPU cores and their frequency in MHz
 #If it doesn't work change -n.1 -> -n1
 
+#source: https://unix.stackexchange.com/questions/264632/what-is-the-correct-way-to-view-your-cpu-speed-on-linux
+
 watch -n.1 "cat /proc/cpuinfo | grep \"^[c]pu MHz\""
